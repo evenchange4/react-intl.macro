159fc2e1781d2f38a04294ef0aa68ec96309ca97
Michael Hsu <evenchange4@gmail.com>
2018-01-25 12:43:35 +0800

    fix(CI): update snapshot (#2)

--- setupTests.js
+++ setupTests.js
@@ -2,7 +2,7 @@
 /**
  * Note: mock for output file
  */
-jest.mock('./src/utils/writeFileSync.js', () => (params, state) => {
+jest.mock('./src/utils/writeFileSync.js', () => ({ data }, state) => {
   const serialize = require('babel-literal-to-ast');
-  state.file.path.node.body.push(serialize(params));
+  state.file.path.node.body.push(serialize(data));
 });
